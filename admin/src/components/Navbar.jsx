0bf509855eb9595da24451160f5606ef535ca936
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2025-01-08 20:50:13 +0300

    commit

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import logo from '../assets/shop-logo.png';
+import { FiLogOut } from "react-icons/fi";
 
 const Navbar = () => {
     return (
         <nav className='px-[5%] py-3 bg-white shadow-md sticky top-0 z-20 flex justify-between items-center'>
             <a href="/">
-                <img src={logo} className="w-16 h-16 " />
+                <img src={logo} className="w-16 h-16 rounded-full object-cover" />
             </a>
 
             <div className='flex items-center justify-between'>
-                <button className="bg-red-400 text-white rounded-full py-2 px-5 hover:bg-red-500">
-                    Logout
-                </button>
+                <FiLogOut size={24} className='cursor-pointer text-red-600' title="logout" />
             </div>
         </nav>
     )
